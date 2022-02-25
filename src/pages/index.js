35b681761f55ec3208145ce6cfe14cca45fcd356
fdlmhmd29 <fadhilmhmd29@gmail.com>
2022-02-26 04:19:50 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import Banner from "../section/banner";
 import Features from "../section/features";
 import Services from "../section/services";
 import Profile from "../section/profile";
-import "react-modal-video/css/modal-video.min.css";
 import "react-multi-carousel/lib/styles.css";
 import TeamSection from "../section/team-section";
 import BlogSection from "../section/blog-section";
