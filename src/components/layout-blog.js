e3ec66533107b1722260863d3f58b7055a3c0a89
fdlmhmd29 <fadhilmhmd29@gmail.com>
2022-02-27 03:42:50 +0700

    Adding global.css

--- src/components/layout-blog.js
+++ src/components/layout-blog.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Footer from "./footer/footer-blog";
 import Header from "../components/header/header-blog";
-import Seo from "./seo";
 
 function Layout({ children }) {
   return (
     <React.Fragment>
-      <Seo title="Blog" />
       <div className="flex flex-col min-h-screen">
         <div className="max-w-3xl mx-auto lg:max-w-5xl w-full">
           <Header />
